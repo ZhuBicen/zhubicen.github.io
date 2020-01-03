10d908aaa36e800456e53d00994372e69ac0a0ad
bzhu <zhubicen@gmail.com>
2020-01-03 20:50:09 +0800

    One script

--- releaseArticleMac.sh
+++ releaseArticleMac.sh
@@ -1,5 +1,28 @@
+cd docs
+git pull origin master
+cd ..
 Hugo
-./bin/HugoUtils
+case "$(uname -s)" in
+   Darwin)
+       echo 'Mac OS X'
+       ./bin/HugoUtils
+     ;;
+
+   Linux)
+     echo 'Linux'
+     ;;
+
+   CYGWIN*|MINGW32*|MSYS*)
+     echo 'MS Windows'
+     ./bin/HugoUtils       
+     ;;
+
+
+   *)
+     echo 'Other OS' 
+     ;;
+esac
+
 cd docs
 git add *
 git commit -m"New Article" -a
